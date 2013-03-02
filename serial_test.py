ec2f1c0037544c093be3af4a99bcd6d56c23d3a2
joshj19 <joshuajetter@gmail.com>
2013-03-02 15:35:11 -0600

    Added newline

--- serial_test.py
+++ serial_test.py
@@ -1,8 +1,10 @@
 import serial
 ser = serial.Serial('/dev/ttyACM0', 9600)
+ser.open()
+ser.flush()
 s = "on"
 while s != "quit":
    input = raw_input('LED~ ')
    print input
-   ser.write(input)
-   ser.flushOutput()
+   ser.write(input + "\n")
+   ser.flush()
